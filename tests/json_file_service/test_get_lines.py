bff0c6cc2bba4b81fb4f3f52339792b31244a3f4
Ula <ula.malin35@gmail.com>
2024-04-08 22:56:56 +0200

    Implemented abstract factory in file/reader.py to retrieve data from txt and json file and convert it to Car object

--- tests/json_file_service/test_get_lines.py
+++ tests/json_file_service/test_get_lines.py
@@ -1,33 +1,35 @@
 import pytest
 from cars.file.service import JsonFileService
+import logging
 
 
 class TestJsonFileServiceGetLinesPathNotCorrect:
     def test_when_path_has_incorrect_extension(self):
         with pytest.raises(AttributeError) as e:
-            JsonFileService.get_lines('test_data/cars_test.png', 'key')
+            JsonFileService().get_lines('test_data/cars_test.png', 'key')
         assert str(e.value) == 'File has incorrect extension'
 
     def test_when_path_not_found(self):
         with pytest.raises(FileNotFoundError) as e:
-            JsonFileService.get_lines('test_data/cars_testt.json', 'key')
+            JsonFileService().get_lines('test_data/cars_testt.json', 'key')
         assert str(e.value).startswith('File not found')
 
     def test_when_key_not_found(self):
         with pytest.raises(AttributeError) as e:
-            JsonFileService.get_lines('test_data/cars_test.json', 'people')
+            JsonFileService().get_lines('test_data/cars_test.json', 'people')
         assert str(e.value).startswith('Key not found')
 
 
 class TestJsonFileServiceGetLinesEmptyContent:
     def test_when_file_has_no_content(self):
-        data = JsonFileService.get_lines('test_data/empty_cars_test.json', 'cars')
+        data = JsonFileService().get_lines('test_data/empty_cars_test.json', 'cars')
         assert len(data) == 0
 
 
 class TestJsonFileServiceGetLinesContentCorrect:
     def test_when_content_present(self):
-        data = JsonFileService.get_lines('test_data/cars_test.json', 'cars')
-        assert len(data) == 2
-        assert list(data[0].values()) == ['AUDI', 9000, 'SILVER', 1500, ['SALLOY WHEELS', 'EVASIVE STEERING']]
-        assert list(data[1].values()) == ['BMW', 300, 'WHITE', 2800, ['AIR', 'KEYLESS ENTRY']]
+        data = JsonFileService().get_lines('test_data/cars_test.json', 'cars')
+        assert len(data) == 3
+        assert list(data[0].values()) == ['AUDI', 9000, 'SILVER', 1500, ['CAMERA']]
+        assert list(data[1].values()) == ['BMW', 3000, 'WHITE', 2800, ['KEYLESS ENTRY']]
+        assert list(data[2].values()) == ['TOYOTA', 4000, 'BLUE', 10000, ['KEYLESS ENTRY', 'BLUETOOTH']]
---
+++ tests/test_data_processor.py
@@ -0,0 +1,32 @@
+from cars.file.service import (
+    DataProcessor,
+    JsonFileService,
+    TxtFileService,
+    CarValidator,
+    CarConverter,
+    FromTxtFileToCar,
+    FromJsonFileToCar,
+)
+from cars.model import Car
+import logging
+from unittest.mock import MagicMock
+
+
+class TestDataProcessor:
+    def test_text_data_processor(self, txt_path_correct_data):
+        txt_data_factory = FromTxtFileToCar()
+        processor = DataProcessor(txt_data_factory)
+        cars = processor.process(txt_path_correct_data, None)
+        assert cars == [
+            Car(model='BMW', price=2000, color='BLUE', mileage=1500, components=['BLUETOOTH', 'ABS'])
+        ]
+
+    def test_json_data_processor(self, json_path_correct_data):
+        json_data_factory = FromJsonFileToCar()
+        processor = DataProcessor(json_data_factory)
+        cars = processor.process(json_path_correct_data, 'cars')
+        assert cars == [
+            Car(model='AUDI', price=9000, color='SILVER', mileage=1500, components=['CAMERA']),
+            Car(model='TOYOTA', price=4000, color='BLUE', mileage=10000, components=['KEYLESS ENTRY', 'BLUETOOTH'])
+
+        ]
