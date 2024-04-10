d56a1f3bd4d778c209374e02d0c1903c88986445
Ula <ula.malin35@gmail.com>
2024-04-10 13:36:00 +0200

    Improved statistics module, one parametrized method for statitics for price and mileage category

--- cars/cars_service/statistics.py
+++ cars/cars_service/statistics.py
@@ -15,29 +15,15 @@ class Statistics:
 class StatisticsService:
     cars: list[Car] = field(default_factory=list)
 
-    def get_statistics_for_mileage(self) -> Statistics:
-        mileage_values = [car.mileage for car in self.cars]
-        avg_value = Decimal(sum(mileage_values) / len(mileage_values))
-        return Statistics(
-            min_v=Decimal(min(mileage_values)),
-            avg_v=avg_value,
-            max_v=Decimal(max(mileage_values))
-        )
-
-    def get_statistics_for_price(self) -> Statistics:
-        price_values = [car.price for car in self.cars]
-        avg_value = Decimal(sum(price_values) / len(price_values))
-        return Statistics(
-            min_v=Decimal(min(price_values)),
-            avg_v=avg_value,
-            max_v=Decimal(max(price_values))
-        )
-
     def get_statistics_for(self, category: Category) -> Statistics:
-        match category:
-            case category.PRICE:
-                return self.get_statistics_for_price()
-            case category.MILEAGE:
-                return self.get_statistics_for_mileage()
-            case _:
-                raise AttributeError('Incorrect category')
+        attribute_name = category.name.lower()
+        if not all(hasattr(car, attribute_name) for car in self.cars):
+            raise AttributeError(f'Invalid category: {category}')
+
+        values = [getattr(car, attribute_name) for car in self.cars]
+        avg_value = Decimal(sum(values) / len(values))
+        return Statistics(
+            min_v=Decimal(min(values)),
+            avg_v=avg_value,
+            max_v=Decimal(max(values))
+        )
