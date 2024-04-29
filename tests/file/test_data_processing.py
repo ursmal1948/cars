8099a24595675a019601aee4f3ab7b4341e043db
Ula <ula.malin35@gmail.com>
2024-04-29 02:02:10 +0200

    tests for factories moved to one module

--- tests/file/test_data_processing.py
+++ tests/file/test_data_processing.py
@@ -1,11 +1,49 @@
-import unittest
 from cars.model import Car
-from cars.file.service import FromTxtFileToCar, FromJsonFileToCar
-from cars.file.service import DataProcessor
-from cars.file.service import DataFactoryType
+from cars.file.service import FromTxtFileToCar, FromJsonFileToCar, DataProcessor, DataFactoryType
 from cars.file.reader import TxtFileService, JsonFileService
 from cars.validator import CarsDataValidator
 from cars.converter import CarsConverter
+import unittest
+
+
+class TestDataFactoryFromTxtFileToCar(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = FromTxtFileToCar()
+
+    def test_create_file_service(self):
+        file_service = self.factory.create_file_service()
+        self.assertIsInstance(file_service, TxtFileService)
+
+    def test_create_validator(self):
+        validator = self.factory.create_validator()
+        self.assertIsInstance(validator, CarsDataValidator)
+        self.assertEqual(validator.model_regex, r'^[A-Z\s]+$')
+        self.assertEqual(validator.colors, ['SILVER', 'BLUE'])
+
+    def test_create_converter(self):
+        converter = self.factory.create_converter()
+        self.assertIsInstance(converter, CarsConverter)
+
+
+class TestDataFactoryFromJsonFileToCar(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = FromJsonFileToCar()
+
+    def test_create_file_service(self):
+        file_service = self.factory.create_file_service()
+        self.assertIsInstance(file_service, JsonFileService)
+
+    def test_create_validator(self):
+        validator = self.factory.create_validator()
+        self.assertIsInstance(validator, CarsDataValidator)
+        self.assertEqual(validator.model_regex, r'^[A-Z\s]+$')
+        self.assertEqual(validator.colors, ['SILVER', 'BLUE'])
+
+    def test_create_converter(self):
+        converter = self.factory.create_converter()
+        self.assertIsInstance(converter, CarsConverter)
 
 
 class TestDataProcessorProcess:
